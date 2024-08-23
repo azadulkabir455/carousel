100f07853376e6a9db2b23286c56fc49a9517092
azad ul kabir <azadulkabir10@gmail.com>
2024-08-24 03:03:08 +0600

    [dev] - carosel addon - done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Carousel from './carousel/Carousel';
 
 function App() {
+
+  const settings = {
+    showArrows: true,
+    responsive: {
+      mobile: {
+        breakpoint: 767,
+        slidesToShow: 1
+      },
+      tablet: {
+        breakpoint: 1024,
+        slidesToShow: 2
+      },
+      desktop: {
+        slidesToShow: 4
+      }
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Carousel</h1>
+      <Carousel settings={settings} >
+        <div>
+          <img src="https://shorturl.at/q00le" alt="img" />
+        </div>
+        <div>
+          <img src="https://shorturl.at/TaYPg" alt="img" />
+        </div>
+        <div>
+          <img src="https://shorturl.at/WKMhn" alt="img" />
+        </div>
+        <div>
+          <img src="https://shorturl.at/Nj76k" alt="img" />
+        </div>
+        <div>
+          <img src="https://shorturl.at/RBRgw" alt="img" />
+        </div>
+        <div>
+          <img src="https://shorturl.at/pK9XT" alt="img" />
+        </div>
+        <div>
+          <img src="https://rb.gy/9qwwgm" alt="img" />
+        </div>
+        <div>
+          <img src="https://rb.gy/krs7ph" alt="img" />
+        </div>
+        <div>
+          <img src="https://rb.gy/vjpoui" alt="img" />
+        </div>
+
+      </Carousel>
+    </>
   );
 }
 
---
+++ src/carousel/Carousel.jsx
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from 'react';
+import "./carousel.css"
+
+const Carousel = ({ settings, children }) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [itemsToShow, setItemsToShow] = useState();
+    const [showIndividualArrow, setShowIndividualArrow] = useState({
+        leftArrow: false,
+        rightArrow: true,
+    });
+
+    //destruction of settings come from props
+    const { showArrows, responsive } = settings;
+
+    //Function for next/prev slider functionality
+    const nextSlide = () => {
+        let endSlidesCount = (children.length - itemsToShow) - 1;
+        if (currentIndex <= endSlidesCount) {
+            setCurrentIndex((prevIndex) => prevIndex + 1);
+            setShowIndividualArrow(prev => ({ ...prev, leftArrow: true }));
+        } else {
+            setShowIndividualArrow(prev => ({ ...prev, rightArrow: false }));
+        }
+    };
+
+    const prevSlide = () => {
+        if (currentIndex > 0) {
+            setCurrentIndex((prevIndex) => (prevIndex - 1));
+            setShowIndividualArrow(prev => ({ ...prev, rightArrow: true }));
+        } else {
+            setShowIndividualArrow(prev => ({ ...prev, leftArrow: false }));
+        }
+    };
+
+    useEffect(() => {
+        // Set item to show on initial load
+        const updateItemsToShow = () => {
+            if (window.innerWidth < responsive.mobile.breakpoint) {
+                setItemsToShow(responsive.mobile.slidesToShow); // Show item on small screens
+            } else if (window.innerWidth < responsive.tablet.breakpoint) {
+                setItemsToShow(responsive.tablet.slidesToShow); // Show items on medium screens
+            } else {
+                setItemsToShow(responsive.desktop.slidesToShow); // Show items on larger screens
+            }
+        };
+
+        updateItemsToShow();
+        window.addEventListener('resize', updateItemsToShow);
+
+        //Cleanup for event listener
+        return () => {
+            window.removeEventListener('resize', updateItemsToShow);
+        };
+    }, [responsive]);
+
+    return (
+        <div className="carousel">
+            {/* Slider Item to show */}
+            <div
+                className="carousel-inner"
+                style={{ transform: `translateX(-${currentIndex * (100 / itemsToShow)}%)` }}
+            >
+                {
+                    children.map((childElement, index) =>
+                        <div
+                            key={index}
+                            className="carousel-item"
+                            style={{ minWidth: `${100 / itemsToShow}%` }}
+                        >
+                            {childElement}
+                        </div>
+                    )
+                }
+            </div>
+            
+            {/* Slider arrow button */}
+            {showArrows &&
+                <>
+                    <button
+                        className={`carousel-control prev ${showIndividualArrow.leftArrow ? "enable" : ""}`}
+                        onClick={prevSlide}>
+                        &#10094;
+                    </button>
+                    <button
+                        className={`carousel-control next ${showIndividualArrow.rightArrow ? "enable" : ""}`}
+                        onClick={nextSlide}>
+                        &#10095;
+                    </button>
+                </>
+            }
+        </div>
+    );
+};
+
+export default Carousel;
